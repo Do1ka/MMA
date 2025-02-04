02f48bb4b36474b6169df6726a8c302ec32f5442
TranVanThao <thaotvhe170862@fpt.edu.vn>
2025-02-05 06:50:27 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native-web";
+
+export default function App(){
+    return(
+        <View>
+            <Text>Helo</Text>
+        </View>
+    )
+}
